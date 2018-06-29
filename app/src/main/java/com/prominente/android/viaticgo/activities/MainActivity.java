bfc2739aadf1748d158709a649f4915eefb4fa21
mobachi <marcelo.obachi@gmail.com>
2018-06-29 15:11:49 -0300

    inicial

---
+++ app/src/main/java/com/prominente/android/viaticgo/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.prominente.android.viaticgo.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.prominente.android.viaticgo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
