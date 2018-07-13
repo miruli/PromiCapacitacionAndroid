6c3629859cc2543d37f5f51d75c9ed9a304abd02
mobachi <marcelo.obachi@gmail.com>
2018-07-13 17:15:34 -0300

    clase 7

--- app/src/main/java/com/prominente/android/viaticgo/activities/MainActivity.java
+++ app/src/main/java/com/prominente/android/viaticgo/activities/MainActivity.java
@@ -2,6 +2,10 @@ package com.prominente.android.viaticgo.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
 
 import com.prominente.android.viaticgo.R;
 
@@ -12,5 +16,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView rvTickets = findViewById(R.id.rv_tickets);
+        rvTickets.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new RvAdapter(new RvAdapterClickLister() {
+            @Override
+            public void onItemClick(View v, int position) {
+                Toast.makeText(MainActivity.this, "Position "+position, Toast.LENGTH_SHORT).show();
+                Ticket t = adapter.getItems().get(position);
+            }
+
+            @Override
+            public void onItemLongClick(View v, int position) {
+
+            }
+        });
+        rvTickets.setAdapter(adapter);
+        adapter.addAll(getItems());
     }
 }
---
+++ app/src/main/java/com/prominente/android/viaticgo/adapters/RvAdapter.java
@@ -0,0 +1,61 @@
+package com.prominente.android.viaticgo.adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.prominente.android.viaticgo.R;
+import com.prominente.android.viaticgo.models.Ticket;
+
+public class RvAdapter extends ArrayRvAdapter<Ticket,RvAdapter.TicketViewHolder>
+{
+    private RvAdapterClickLister clickLister;
+
+    protected static class TicketViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+    {
+        private RvAdapterClickLister clickLister;
+
+        TextView tvTitle;
+        TextView tvSubtitle;
+
+        public TicketViewHolder(View itemView, RvAdapterClickLister listener)
+        {
+            super(itemView);
+
+            clickLister = listener;
+            itemView.setOnClickListener(this);
+            tvTitle = itemView.findViewById(R.id.tv_ticket_rv_title);
+            tvSubtitle = itemView.findViewById(R.id.tv_ticket_rv_subtitle);
+        }
+
+        @Override
+        public void onClick(View v)
+        {
+            clickLister.onItemClick(v, getLayoutPosition());
+        }
+    }
+
+    public RvAdapter(RvAdapterClickLister listener)
+    {
+        clickLister = listener;
+    }
+
+    @NonNull
+    @Override
+    public TicketViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.ticket_rv_item, parent, false);
+        return new TicketViewHolder(v, clickLister);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TicketViewHolder holder, int position)
+    {
+        Ticket t = getItems().get(position);
+        holder.tvTitle.setText(t.getDescription());
+        holder.tvSubtitle.setText(String.valueOf(t.getAmount()));
+    }
+}
